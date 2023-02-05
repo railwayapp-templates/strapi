126e53ca81283c5acfc013c1e13006fc84d0db5a
glatour <guillaume.latour@gmail.com>
2023-02-04 23:15:21 -0500

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'e4350524b0689fc3f46e2407dae950e2'),
+    secret: env('ADMIN_JWT_SECRET', 'FLXpl7YLTq+HmezJh3rS+qYWw6y46mplEmS3aOiq7hk='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'DfotF/dgKzxxwnnN1brOYW0XSHijdSTv70Lrh/yurcc=),
   },
 
 });
