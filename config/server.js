ba06246235d8b0a2c846c2c35ad5e6779fedcd2f
Brody <brodyover@gmail.com>
2023-11-14 00:01:49 -0500

    detect railway and swap urls around

--- config/server.js
+++ config/server.js
@@ -1,8 +1,12 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-  url: env('URL')
-});
+module.exports = ({ env }) => {
+  const url = (process.env.HOSTNAME == "railway") ? process.env.URL : 'http://127.0.0.1:1337';
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array("APP_KEYS"),
+    },
+    url: url
+  }
+}
