55def6c55d920dbad430efbc924d166460be081e
markokara <markokara07@gmail.com>
2025-03-11 17:16:56 +0300

    server.js

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'https://strapi-production-1778.up.railway.app/'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
