b2424aa916d882bfa6ed1816a262d30d331defa5
markokara <markokara07@gmail.com>
2025-03-12 12:02:25 +0300

    server.js

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url: env('PUBLIC_URL', 'https://strapi-developer-39ea.up.railway.app/'),
+  url: env('PUBLIC_URL', 'https://strapi-development-7a4d.up.railway.app/'),
   host: '0.0.0.0',
   port: env.int('PORT', 1337),
   environment: env('NODE_ENV', 'development'), // Development modunu zorunlu yap!
