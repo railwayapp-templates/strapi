d8afb9d808a5f464e7c0066f366583295457d744
chloe gregoire <chle.gregire@gmail.com>
2025-06-30 11:24:03 +0200

    config db template

--- config/database.js
+++ config/database.js
@@ -24,21 +24,12 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) ?{
-          rejectUnauthorized: false,
-          ca: false,
-          key: false,
-          cert: false
-        } : false,
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+            connectionString: env('DATABASE_URL')
+        },
+        debug: true,
+        pool: { min: 0, max: 7 },
     },
+
     sqlite: {
       connection: {
         filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
