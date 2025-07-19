be2bd457255f7ccd9da73e7c4643dd0947d5cab7
mingxingmeng <mingxingmeng93@gmail.com>
2025-07-19 22:44:33 +0800

    创建Articles表

--- config/env/development/server.js
+++ config/env/development/server.js
@@ -1,3 +1,7 @@
 module.exports = ({ env }) => ({
-    url: `http://127.0.0.1:1337`
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
 });
